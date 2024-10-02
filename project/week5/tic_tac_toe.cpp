6c3cb87297a7b7462be21c0cb313f6ddc9d5d8b4
kim214958 <kimgnltmd@naver.com>
2024-10-02 22:16:02 +0900

    2-d Array and Tic Tac Toe fix more

--- project/week5/tic_tac_toe.cpp
+++ project/week5/tic_tac_toe.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 
 int main(){
-    const int numCell = 3;
+    const int numCell = 3; // 보드 한 줄의 칸 수
     char board[numCell][numCell]{}; // 보드판 설정
     int x, y; // 사용자에게 입력받는 x,y좌표를 저장할 변수
     int countspace = 0; // 보드판의 채워진 칸을 확인하는 변수
+    
+    // 게임을 하는 인원 수 3명 이상이면 switch문의 case를 인원 수 만큼 만들어야한다.
+     int players = 2; 
 
     //보드판 초기화
     for(x=0; x<numCell; x++){
@@ -19,7 +22,7 @@ int main(){
     char currentUser = 'X'; //현재 유저의 돌을 저장하기 위한 문자 변수
     while(true){
         //1. 누구 차례인지 출력
-        switch(k%2){
+        switch(k%players){
             case 0:
                 cout <<"첫번째 유저(X)의 차례입니다 -> ";
                 currentUser = 'X';
@@ -28,6 +31,10 @@ int main(){
                 cout <<"두번째 유저(O)의 차례입니다 -> ";
                 currentUser = 'O';
                 break;
+            /* case n: // 플레이어가 늘어나면 추가하시오
+                cout <<"n번째 유저(N)의 차례입니다 -> ";
+                currentUser = 'N';
+                break; */
         }
 
         // 2. 좌표 입력 받기 (직관성을 위해 x와 y를 따로따로 입력받게 만들었다.)
@@ -38,7 +45,7 @@ int main(){
         
 
         // 3.입력받은 좌표의 유효성 체크
-        if(x>= numCell || y>= numCell){
+        if(x < 0 || y < 0 || x >= numCell || y >= numCell){
             cout << x << ", " << y << ": ";
             cout << " x 와 y 둘 중 하나가 칸을 벗어납니다." <<endl;
             continue;
@@ -54,7 +61,10 @@ int main(){
 
         // 5. 현재 보드 판 출력
         for (int i = 0; i <numCell; i++){
-            cout << "---|---|---" << endl;
+            for(int i = 0; i<numCell-1; i++){
+                cout << "---|";
+                }
+            cout << "---" << endl;
             for (int j =0; j < numCell; j++){
                 cout << board[i][j];
                 if(j == numCell-1){
@@ -64,7 +74,10 @@ int main(){
             }
             cout << endl;
         }
-        cout << "---|---|---" << endl;
+        for(int i = 0; i<numCell-1; i++){
+                cout << "---|";
+                }
+            cout << "---" << endl;
         k++;
         countspace++;   // 표시된 공간을 카운트
 
@@ -94,17 +107,28 @@ int main(){
                 }
             }
         }
-        // 6-3. 빙고 시 승자 출력 후 종료 (대각선)
+        // 6-3. 빙고 시 승자 출력 후 종료 (대각선 왼쪽위 -> 오른쪽 아래)
         for(int i =0; i<numCell; i++){
             if(board[i][i] != currentUser){
                 break;
             }
             if(i == numCell-1){
-                cout <<"대각선 한 줄이 채워졌습니다."<< endl;
+                cout <<"대각선 왼쪽 위 -> 오른쪽 아래 한 줄이 채워졌습니다."<< endl;
                 cout<<"승자는 "<<currentUser<<" 입니다." <<endl;
                 return 0;
             }
         }
+        // 6-4. 빙고 시 승자 출력 후 종료 (대각선 오른쪽 위 -> 왼쪽 아래)
+        for(int i = 0; i < numCell; i++) {
+          if(board[i][numCell - 1 - i] != currentUser) {
+             break;
+             }
+            if(i == numCell - 1) {
+                cout << "대각선 오른쪽 위 -> 왼쪽 아래 한 줄이 채워졌습니다." << endl;
+                cout << "승자는 " << currentUser << " 입니다." << endl;
+                return 0;
+             }
+}
 
         // 7. 모든 칸이 다 차면 종료
         if(countspace == (numCell)*(numCell)){
--- project/week5/tic_tac_toe3.cpp
+++ project/week5/tic_tac_toe3.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 
 int main(){
-    const int numCell = 3;  // 보드판 칸 수
+    const int numCell = 3; // 보드 한 줄의 칸 수
     char board[numCell][numCell]{}; // 보드판 설정
     int x, y; // 사용자에게 입력받는 x,y좌표를 저장할 변수
     int countspace = 0; // 보드판의 채워진 칸을 확인하는 변수
+    
+    // 게임을 하는 인원 수 3명 이상이면 switch문의 case를 인원 수 만큼 만들어야한다.
+     int players = 3; 
 
     //보드판 초기화
     for(x=0; x<numCell; x++){
@@ -19,7 +22,7 @@ int main(){
     char currentUser = 'X'; //현재 유저의 돌을 저장하기 위한 문자 변수
     while(true){
         //1. 누구 차례인지 출력
-        switch(k%3){
+        switch(k%players){
             case 0:
                 cout <<"첫번째 유저(X)의 차례입니다 -> ";
                 currentUser = 'X';
@@ -28,8 +31,8 @@ int main(){
                 cout <<"두번째 유저(O)의 차례입니다 -> ";
                 currentUser = 'O';
                 break;
-            case 2:
-                cout <<"세번째 유저(V)의 차례입니다 -> ";
+            case 2: // 플레이어가 늘어나면 추가하시오
+                cout <<"n번째 유저(V)의 차례입니다 -> ";
                 currentUser = 'V';
                 break;
         }
@@ -41,8 +44,8 @@ int main(){
         cin >> y ;
         
 
-        // 3.입력받은 좌표의 유효성 체크
-        if(x>= numCell || y>= numCell){
+        // 3.입력받은 좌표의 유효성 체크(음수 확인도 추가하였다.)
+        if(x < 0 || y < 0 || x >= numCell || y >= numCell){
             cout << x << ", " << y << ": ";
             cout << " x 와 y 둘 중 하나가 칸을 벗어납니다." <<endl;
             continue;
@@ -56,9 +59,12 @@ int main(){
         // 4. 입력받은 좌표에 현재 유저의 돌 놓기
         board[x][y] = currentUser;
 
-        // 5. 현재 보드 판 출력
+        // 5. 현재 보드 판 출력 (보드 칸 수가 늘어도 작동하도록 고침)
         for (int i = 0; i <numCell; i++){
-            cout << "---|---|---" << endl;
+            for(int i = 0; i<numCell-1; i++){
+                cout << "---|";
+                }
+            cout << "---" << endl;
             for (int j =0; j < numCell; j++){
                 cout << board[i][j];
                 if(j == numCell-1){
@@ -68,7 +74,10 @@ int main(){
             }
             cout << endl;
         }
-        cout << "---|---|---" << endl;
+        for(int i = 0; i<numCell-1; i++){
+                cout << "---|";
+                }
+            cout << "---" << endl;
         k++;
         countspace++;   // 표시된 공간을 카운트
 
@@ -98,17 +107,28 @@ int main(){
                 }
             }
         }
-        // 6-3. 빙고 시 승자 출력 후 종료 (대각선)
+        // 6-3. 빙고 시 승자 출력 후 종료 (대각선 왼쪽위 -> 오른쪽 아래)
         for(int i =0; i<numCell; i++){
             if(board[i][i] != currentUser){
                 break;
             }
             if(i == numCell-1){
-                cout <<"대각선 한 줄이 채워졌습니다."<< endl;
+                cout <<"대각선 왼쪽위 -> 오른쪽 아래 한 줄이 채워졌습니다."<< endl;
                 cout<<"승자는 "<<currentUser<<" 입니다." <<endl;
                 return 0;
             }
         }
+        // 6-4. 빙고 시 승자 출력 후 종료 (대각선 오른쪽 위 -> 왼쪽 아래)
+        for(int i = 0; i < numCell; i++) {
+          if(board[i][numCell - 1 - i] != currentUser) {
+             break;
+             }
+            if(i == numCell - 1) {
+                cout << "대각선 오른쪽 위 -> 왼쪽 아래 한 줄이 채워졌습니다." << endl;
+                cout << "승자는 " << currentUser << " 입니다." << endl;
+                return 0;
+             }
+}
 
         // 7. 모든 칸이 다 차면 종료
         if(countspace == (numCell)*(numCell)){
